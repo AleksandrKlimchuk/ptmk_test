d6b3b179f95023b61a4725aef02cdf1b5e810a2b
AleksandrKlimchuk <1vipe234@gmail.com>
2023-02-16 17:42:23 +0300

    Add unique humans selection method in repository

--- src/main/java/ptmk/task/repository/HumanRepository.java
+++ src/main/java/ptmk/task/repository/HumanRepository.java
@@ -2,8 +2,13 @@ package ptmk.task.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import ptmk.task.dto.HumanDTO;
+import ptmk.task.model.Gender;
 import ptmk.task.model.Human;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -24,5 +29,22 @@ public class HumanRepository {
     public void insertHuman(Human human) {
         final String sql = "INSERT INTO human (fullname, birthday, gender) VALUES (?, ?, ?);";
         jdbc.update(sql, human.getFullName(), human.getBirthday(), human.getGender().getState());
+    }
+
+    public List<HumanDTO> selectHumanWithUniqueFullnameAndBirthdayOrderByFullname() {
+        final String sql = "select distinct on (fullname, birthday) " +
+                "fullname, birthday, gender, date_part('year', age(CURRENT_DATE, birthday)) as age " +
+                "from human order by fullname;";
+
+        RowMapper<HumanDTO> humanDTORowMapper = (
+                (rs, rowNum) -> HumanDTO.builder()
+                        .fullname(rs.getString("fullname"))
+                        .birthday(rs.getDate("birthday"))
+                        .gender(Gender.of(rs.getInt("gender")))
+                        .ageInYears(rs.getInt("age"))
+                        .build()
+        );
+
+        return jdbc.query(sql, humanDTORowMapper);
     }
 }
