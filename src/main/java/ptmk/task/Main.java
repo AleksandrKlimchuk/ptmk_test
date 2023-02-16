c6f635957108a42cb422b5eae8b77de91526dfb4
AleksandrKlimchuk <1vipe234@gmail.com>
2023-02-16 12:13:13 +0300

    Add maven settings and Main class

---
+++ src/main/java/ptmk/task/Main.java
@@ -0,0 +1,11 @@
+package ptmk.task;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args[0].equals("1")) {
+            System.out.println("1 point");
+        }
+        System.out.println("Working from console");
+    }
+}
